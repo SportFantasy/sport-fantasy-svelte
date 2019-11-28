94a89b20b491ec85292beedb3ad29360cf5e2a81
Marin <marinbgd@gmail.com>
2019-11-28 10:18:56 +0100

    Refactors - moves fetching games data from AuthRoot to Home page

--- src/AuthRoot.svelte
+++ src/AuthRoot.svelte
@@ -6,30 +6,6 @@
   import Welcome from './common/Welcome.svelte'
   import AUTH_APP_ROUTES from './authenticatedRoutes'
   import { authStore } from './auth/auth.store'
-  import { gamesStore } from './common/games.store'
-
-
-  import { getAllUsers } from './common/db/users.js'
-  import { getAllGames } from './common/db/games.js'
-  import { getAllGameTypes } from './common/db/gameTypes.js'
-  import { addUsersToGames, addGameTypesToGames } from './common/games.store.helper'
-
-  const getGamesData = () => {
-    return Promise.all([getAllUsers(), getAllGameTypes(), getAllGames()])
-  }
-
-  const setGamesStore = ([users, gameTypes, games]) => {
-    const gamesWithUsers = addUsersToGames(games, users)
-    const gamesWithTypes = addGameTypesToGames(gamesWithUsers, gameTypes)
-
-    gamesStore.setUsers(users)
-    gamesStore.setGameTypes(gameTypes)
-    gamesStore.setGames(gamesWithTypes)
-  }
-
-  onMount(() => {
-    getGamesData().then(setGamesStore)
-  })
 </script>
 
 <TopNavigation isAuthenticated={$authStore.isAuthenticated} />
--- src/home/Home.svelte
+++ src/home/Home.svelte
@@ -1,8 +1,39 @@
 <script>
-  import { gamesStore } from '../common/games.store'
-  import { getGamesByGameTypeId } from '../common/games.store.helper'
+  import Router from 'svelte-spa-router'
+  import { onMount } from 'svelte';
+
   import GameTypeSelector from './GameTypeSelector.svelte'
   import GameCard from './GameCard.svelte'
+
+  import { authStore } from '../auth/auth.store'
+  import { gamesStore } from '../common/games.store'
+
+  import { getAllUsers } from '../common/db/users.js'
+  import { getAllGames } from '../common/db/games.js'
+  import { getAllGameTypes } from '../common/db/gameTypes.js'
+  import {
+    addUsersToGames,
+    addGameTypesToGames,
+    getGamesByGameTypeId,
+  } from '../common/games.store.helper'
+
+
+  const getGamesData = () => Promise.all([
+    getAllUsers(), getAllGameTypes(), getAllGames(),
+  ])
+
+  const setGamesStore = ([users, gameTypes, games]) => {
+    const gamesWithUsers = addUsersToGames(games, users)
+    const gamesWithTypes = addGameTypesToGames(gamesWithUsers, gameTypes)
+
+    gamesStore.setUsers(users)
+    gamesStore.setGameTypes(gameTypes)
+    gamesStore.setGames(gamesWithTypes)
+  }
+
+  onMount(() => {
+    getGamesData().then(setGamesStore)
+  })
 
   export let params = {}
 
