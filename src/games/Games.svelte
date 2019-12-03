7746c86d22e581f085a661e78ca05a2382034f56
Marin <marinbgd@gmail.com>
2019-12-03 16:57:29 +0100

    Refactors - all initial data loading moved in AuthRoot component

--- src/games/Games.svelte
+++ src/games/Games.svelte
@@ -1,53 +1,19 @@
 <script>
-  import Router from "svelte-spa-router";
-  import { onMount } from "svelte";
-
   import GameTypeSelector from "./GameTypeSelector.svelte";
   import GameCard from "./GameCard.svelte";
   import Score from "./Score.svelte";
-
-  import { authStore } from "../auth/auth.store";
   import { gamesStore } from "../common/stores/games.store";
-  import { usersStore } from "../common/stores/users.store";
-
-  import { getAllUsers } from "../common/db/users.js";
-  import { getAllGames } from "../common/db/games.js";
-  import { getAllGameTypes } from "../common/db/gameTypes.js";
   import {
-    addUsersToGames,
-    addGameTypesToGames,
     getGamesByGameTypeId,
-    addResultAndWinnerToGames,
     filterOutUnconfirmedGames,
   } from "../common/stores/games.store.helper";
 
-  const getGamesData = () =>
-    Promise.all([getAllUsers(), getAllGameTypes(), getAllGames()]);
 
   let gamesArrayList = [];
-
-  const setGamesStore = ([users, gameTypes, games]) => {
-    // console.log(games)
-    const gamesWithUsers = addUsersToGames(games, users);
-    // console.log(gamesWithUsers)
-    const gamesWithTypes = addGameTypesToGames(gamesWithUsers, gameTypes);
-    // console.log(gamesWithTypes)
-    const gamesWithWinners = addResultAndWinnerToGames(gamesWithTypes);
-    // console.log(gamesWithWinners)
-
-    usersStore.setUsers(users);
-    gamesStore.setGameTypes(gameTypes);
-    gamesStore.setGames(gamesWithWinners);
-  };
-
-  onMount(() => {
-    getGamesData().then(setGamesStore);
-  });
 
   export let params = {};
 
   $: selectedGameTypeId = params.gameTypeId;
-
 
   $: gamesArr = () => {
     const confirmedGames = filterOutUnconfirmedGames($gamesStore.games)
