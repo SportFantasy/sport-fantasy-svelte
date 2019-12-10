62e87a74bbb11cf722b415950341ed6ca5f77459
Marin <marinbgd@gmail.com>
2019-12-10 16:16:46 +0100

    Fixes - storybook UserWidget

--- stories/UserWidget.stories.js
+++ stories/UserWidget.stories.js
@@ -4,21 +4,22 @@ export default { title: 'User/UserWidget' }
 
 
 const USER_DUMMY_DATA = {
-  displayName: 'Testing Name',
-  email: 'testing@griddynamics.com',
-  id: '11111111111111111',
-  createdAt: 1575456896893,
+    displayName: 'Testing Name',
+    email: 'testing@griddynamics.com',
+    id: '11111111111111111',
+    createdAt: 1575456896893,
+    lastLoginAt: 1575457896893,
 }
 
-export const withoutUserProp = () => ({
-  Component: UserWidget,
-})
+export const withoutUserProp = () => ( {
+    Component: UserWidget,
+} )
 
-export const withUserProp = () => ({
-  Component: UserWidget,
-  props: {
-    user: USER_DUMMY_DATA,
-    confirmedGamesNo: 10,
-    unConfirmedGamesNo: 20,
-  },
-})
+export const withUserProp = () => ( {
+    Component: UserWidget,
+    props: {
+        user: USER_DUMMY_DATA,
+        confirmedGamesNo: 10,
+        unConfirmedGamesNo: 20,
+    },
+} )
